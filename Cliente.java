72d35f713ee6878c74ffda44ee132f7233cc26c4
DESKTOP-ODVG4OU\Kevin <kevponce420@gmail.com>
2022-06-13 18:05:17 -0600

    Terminado agregacion

---
+++ Cliente.java
@@ -0,0 +1,55 @@
+import java.util.List;
+
+public class Cliente {
+    private  String nombre;
+    private String edad;
+    private Empresa empresa;
+    private static List<Cliente> clientes;
+
+    public Cliente(String nombre , String edad,Empresa empresa){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.empresa=empresa;
+    }
+
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getEdad(){
+        return edad;
+    }
+    public void setEdad(String edad){
+        this.edad=edad;
+    }
+
+
+
+    public void setEmpresa(Empresa empresa){
+        this.empresa=empresa;
+    }
+
+    public  Empresa  getEmpresa(){
+        return empresa;
+    }
+
+    
+    public static void agregarCliente(Cliente Objcliente){
+        
+        clientes.add(Objcliente);
+
+    }
+
+    public String toString(){
+        return nombre;
+    }
+  
+}
+
+
+
+
--- Main.java
+++ Main.java
@@ -3,24 +3,39 @@ import java.util.*;
 public class Main {
 
     static LinkedList<Empleado> empleados = new LinkedList<>();
-
+    static LinkedList<Cliente> clientes = new LinkedList<>();
     
     
     public static void main(String[] args){
-    Empresa miEmpresa = new Empresa("HCH", empleados);
+       
+     
+        
+     Empresa miEmpresa = new Empresa("HCH", empleados);
      Empleado empleado1 = new Empleado("Ramirez", (float) 5000);
      Empleado empleado2 = new Empleado("Carlos", (float) 5000);
+     Empleado empleado3 = new Empleado("Fernando",(float) 6000);
      
+
+     Cliente cliente1 = new Cliente("Kevin","20",miEmpresa);
+     Cliente.agregarCliente(cliente1);
+
+    clientes.forEach(System.out::println);
+       
+
     List<Empleado> emp = miEmpresa.getEmpleados();
 
         miEmpresa.contratar(empleado1);
         miEmpresa.contratar(empleado2);
+        miEmpresa.contratar(empleado3);
 
         for (Empleado empleado : emp) {
-            System.out.println(empleado.getNombre());
+            System.out.println("Nombre de empleado "+empleado.getNombre() +"   Sueldo : "+empleado.getSueldo());
         }
        
     }
+
+
+   
 
 
 }
